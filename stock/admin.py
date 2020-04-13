17b6c6e97aa6f5a206063e786ad52c10a1278748
Sinchard <sinchard420@gmail.com>
2020-04-13 16:23:41 +0800

    define Warehouse display information

--- stock/admin.py
+++ stock/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from stock.models import Warehouse
+
+
+@admin.register(Warehouse)
+class WarehouseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'location', 'check', 'create_date',
+                    'modify_date', 'description', 'attach')
+    # 文章列表里显示想要显示的字段
+    list_per_page = 50
+    # 满50条数据就自动分页
+    ordering = ('-modify_date', )
+    #后台数据列表排序方式
+    list_display_links = ('id', 'name')
+    # 设置哪些字段可以点击进入编辑界面
