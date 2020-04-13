6a7a1a910cb2fc00f30b493d1aed99ee8d90e3e3
Sinchard <sinchard420@gmail.com>
2020-04-13 16:09:02 +0800

    add media config

--- config/settings.py
+++ config/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -119,4 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL =  os.path.join(BASE_DIR, 'media')
