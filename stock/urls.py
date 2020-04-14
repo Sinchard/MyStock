acc662edeb05636ee0d6b62fcd11c02d1db88a36
Sinchard <sinchard420@gmail.com>
2020-04-14 13:04:44 +0800

    add add-warehouse url

--- stock/urls.py
+++ stock/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from stock import views
-	
+
 app_name = 'stock'
 
 urlpatterns = [
     #path('', views.index, name='index'),
     path('', views.show_warehouse, name='show_warehouse'),
+    path('add', views.add_warehouse, name='add_warehouse'),
 ]
