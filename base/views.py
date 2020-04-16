ee8743a9327fcc64f73d5247bbf2114892104ee5
Sinchard  Smith <a52581120@163.com>
2020-04-16 23:08:52 +0800

    add show and edit wordbook views

--- base/views.py
+++ base/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
-# Create your views here.
+from base.models import Wordbook
+
+
+def show_wordbook(request):
+    wordbooks = wordbook.objects.all()
+    context_dict = {}
+    context_dict["wordbooks"] = wordbooks
+    return render(request, 'base/wordbook.html', context_dict)
+
+
+def add_wordbook(request):
+    form = wordbookForm()
+
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = wordbookForm(request.POST)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+            # Now that the category is saved, we could confirm this.
+            # For now, just redirect the user back to the index view.
+            return redirect('/base/')
+        else:
+            # The supplied form contained errors -
+            # just print them to the terminal.
+            print(form.errors)
+
+    # Will handle the bad form, new form, or no form supplied cases.
+    # Render the form with error messages (if any).
+    return render(request, 'base/add_wordbook.html', {'form': form})
