f62461a9647ef45ea6a0a8f602ca469171ca1b89
Sinchard  Smith <a52581120@163.com>
2020-04-16 21:57:59 +0800

    move wordbook from property to base app

--- base/models.py
+++ base/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+from stock.models import CommonInfo
+
+
+class Wordbook(CommonInfo):
+    name = models.CharField(max_length=30,
+                            unique=True,
+                            verbose_name=u"名称",
+                            db_index=True)
+    parent = models.ForeignKey("self",
+                               null=True,
+                               verbose_name=u"",
+                               on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+
+    def get_type_display(self):
+        return self.name
+
+    def dict(self):
+        d = {'id': self.id, 'name': self.name}
+        d.update(super(DeviceType, self).dict())
+        return d
+
+    class Meta:
+        verbose_name = u'字典表'
+        verbose_name_plural = u'字典表'
--- property/models.py
+++ property/models.py
@@ -1,29 +1,2 @@
 from django.db import models
 
-from stock.models import CommonInfo
-
-
-class Wordbook(CommonInfo):
-    name = models.CharField(max_length=30,
-                            unique=True,
-                            verbose_name=u"名称",
-                            db_index=True)
-    parent = models.ForeignKey("self",
-                               null=True,
-                               verbose_name=u"父类",
-                               on_delete=models.SET_NULL)
-
-    def __str__(self):
-        return self.name
-
-    def get_type_display(self):
-        return self.name
-
-    def dict(self):
-        d = {'id': self.id, 'name': self.name}
-        d.update(super(DeviceType, self).dict())
-        return d
-
-    class Meta:
-        verbose_name = u'字典表'
-        verbose_name_plural = u'字典表'
