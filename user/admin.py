c5df0df6a92f4f137cf9b5af840984be9a43edd5
Sinchard <sinchard420@gmail.com>
2020-04-17 16:40:12 +0800

    add userprofile model to admin page

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from user.models import UserProfile
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'name', 'department', 'team', 'phone',
+                    'mobile', 'mod_date')
+    # 文章列表里显示想要显示的字段
+    list_per_page = 50
+    # 满50条数据就自动分页
+    ordering = ('-mod_date', )
+    #后台数据列表排序方式
+    list_display_links = ('id', 'name')
+    # 设置哪些字段可以点击进入编辑界面
