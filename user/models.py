28fd03880e1168ee5dc47ea61ba85c02b89d9acf
Sinchard <sinchard420@gmail.com>
2020-04-17 15:54:35 +0800

    UserProfile model used to store user extra infor

--- user/models.py
+++ user/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from base.models import Wordbook
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,
+                                on_delete=models.CASCADE,
+                                related_name='profile')
+    department = models.ForeignKey(Wordbook,
+                                   on_delete=models.SET_NULL,
+                                   blank=True,
+                                   null=True)
+    team = models.ForeignKey(Wordbook,
+                             on_delete=models.SET_NULL,
+                             blank=True,
+                             null=True)
+    telephone = models.CharField('电话', max_length=50, blank=True, null=True)
+    mod_date = models.DateTimeField('Last modified', auto_now=True)
+
+    class Meta:
+        verbose_name = '用户信息'
+
+    def __str__(self):
+        return self.user.__str__()
