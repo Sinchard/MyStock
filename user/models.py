6875d494bf688cf38dc0e65b038800b4218bdd7b
Sinchard <sinchard420@gmail.com>
2020-04-17 16:38:55 +0800

    change column from telephone to mobile, add column phone

--- user/models.py
+++ user/models.py
@@ -8,15 +8,19 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User,
                                 on_delete=models.CASCADE,
                                 related_name='profile')
+    name = models.CharField('姓名', max_length=50, blank=True, null=True)
     department = models.ForeignKey(Wordbook,
                                    on_delete=models.SET_NULL,
+                                   related_name='dep_users',
                                    blank=True,
                                    null=True)
     team = models.ForeignKey(Wordbook,
                              on_delete=models.SET_NULL,
+                             related_name='team_users',
                              blank=True,
                              null=True)
-    telephone = models.CharField('电话', max_length=50, blank=True, null=True)
+    phone = models.CharField('固定电话', max_length=50, blank=True, null=True)
+    mobile = models.CharField('电话', max_length=50, blank=True, null=True)
     mod_date = models.DateTimeField('Last modified', auto_now=True)
 
     class Meta:
