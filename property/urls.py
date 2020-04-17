b92e785e860858716293f59a1c03b97a319c2a84
Sinchard <sinchard420@gmail.com>
2020-04-17 12:39:34 +0800

    empty the url config

--- property/urls.py
+++ property/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
-from base import views
+from base.views import CategoryList, CategoryCreate, CategoryUpdate, CategoryDelete
 
 app_name = 'property'
 
-urlpatterns = [
-    #path('', views.index, name='index'),
-    path('wordbooks', views.show_wordbook, name='wordbooks'),
-    path('wordbooks/add', views.add_wordbook, name='add_wordbook'),
-    path('categories', views.CategoryList.as_view(), name='categories'),
-]
+urlpatterns = []
