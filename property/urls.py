b96c098cf538644cf5299dd61ac836b44d0e3a3a
Sinchard  Smith <a52581120@163.com>
2020-04-19 08:24:09 +0800

    add device operation url

--- property/urls.py
+++ property/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from base.views import CategoryList, CategoryCreate, CategoryUpdate, CategoryDelete
+from property.views import DeviceList, DeviceCreate, DeviceUpdate, DeviceDelete
 
 app_name = 'property'
 
-urlpatterns = []
+urlpatterns = [
+    path('devices', DeviceList.as_view(), name='devices'),
+    path('device/add', DeviceCreate.as_view(), name='add_device'),
+    path('device/<int:pk>/', DeviceUpdate.as_view(), name='edit_device'),
+    path('device/<int:pk>/delete',
+         DeviceDelete.as_view(),
+         name='delete_device'),
+]
