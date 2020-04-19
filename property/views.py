58d5f862bc9a4b371f388c39b48bb0ba9e7bca13
Sinchard  Smith <a52581120@163.com>
2020-04-19 18:36:01 +0800

    add material operation view

--- property/views.py
+++ property/views.py
@@ -3,8 +3,8 @@ from django.views.generic import ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.urls import reverse_lazy
 
-from property.models import Device
-from property.forms import DeviceForm
+from property.models import Device, Material
+from property.forms import DeviceForm, MaterialForm
 
 
 class DeviceList(ListView):
@@ -24,3 +24,22 @@ class DeviceUpdate(UpdateView):
 class DeviceDelete(DeleteView):
     model = Device
     success_url = reverse_lazy('property:devices')
+
+
+class MaterialList(ListView):
+    model = Material
+
+
+class MaterialCreate(CreateView):
+    model = Material
+    form_class = MaterialForm
+
+
+class MaterialUpdate(UpdateView):
+    model = Material
+    form_class = MaterialForm
+
+
+class MaterialDelete(DeleteView):
+    model = Material
+    success_url = reverse_lazy('property:Materials')
