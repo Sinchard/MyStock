8978ce7a7b5f36e3634782ee0e408a7a7f2fde44
Sinchard  Smith <a52581120@163.com>
2020-04-19 18:35:10 +0800

    define Material Form

--- property/forms.py
+++ property/forms.py
@@ -1,9 +1,15 @@
 from django.forms import ModelForm
 
-from property.models import Device
+from property.models import Device, Material
 
 
 class DeviceForm(ModelForm):
     class Meta:
         model = Device
+        exclude = []
+
+
+class MaterialForm(ModelForm):
+    class Meta:
+        model = Material
         exclude = []
