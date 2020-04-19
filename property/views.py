72e4d621698a24b6950b94dba994ebc1b0430f7f
Sinchard  Smith <a52581120@163.com>
2020-04-19 08:24:24 +0800

    add device operation views

--- property/views.py
+++ property/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from property.models import Device
+from property.forms import DeviceForm
+
+
+class DeviceList(ListView):
+    model = Device
+
+
+class DeviceCreate(CreateView):
+    model = Device
+    form_class = DeviceForm
+
+
+class DeviceUpdate(UpdateView):
+    model = Device
+    form_class = DeviceForm
+
+
+class DeviceDelete(DeleteView):
+    model = Device
+    success_url = reverse_lazy('property:devices')
