dad380f32bde2eeeb33f3405f37630a5737dca9d
Sinchard  Smith <a52581120@163.com>
2020-04-19 18:34:47 +0800

    register Material model to admin page

--- property/admin.py
+++ property/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
 
-from property.models import Device
+from property.models import Device, Material
 
 
 @admin.register(Device)
 class DeviceAdmin(admin.ModelAdmin):
     list_display = ('id', 'brand', 'model', 'sn', 'classification', 'model', 'value', 'asset', 'sap', 'status', 'warehouse', 'location', 'create_date', 'modify_date',
+                    'description', 'attach')
+    # 文章列表里显示想要显示的字段
+    list_per_page = 50
+    # 满50条数据就自动分页
+    ordering = ('-modify_date', )
+    # 后台数据列表排序方式
+    list_display_links = ('id', )
+    # 设置哪些字段可以点击进入编辑界面
+
+
+@admin.register(Material)
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ('id', 'brand', 'classification', 'account', 'warehouse', 'create_date', 'modify_date',
                     'description', 'attach')
     # 文章列表里显示想要显示的字段
     list_per_page = 50
