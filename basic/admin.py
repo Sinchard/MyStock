2b82f841b9cb16d273576e88f940732d64c7877f
Sinchard  Smith <a52581120@163.com>
2020-04-21 23:03:58 +0800

    add new wordbook to admin page

--- basic/admin.py
+++ basic/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from basic.models import Category, Wordbook, CategoryShip
+from basic.models import Category, Wordbook, WordbookShip
 
 
 @admin.register(Category)
@@ -11,32 +11,31 @@ class CategoryAdmin(admin.ModelAdmin):
     list_per_page = 50
     # 满50条数据就自动分页
     ordering = ('-modify_date', )
-    #后台数据列表排序方式
+    # 后台数据列表排序方式
     list_display_links = ('id', 'name')
     # 设置哪些字段可以点击进入编辑界面
 
 
 @admin.register(Wordbook)
 class WordbookAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'parent', 'check', 'create_date',
+    list_display = ('id', 'name', 'check', 'create_date',
                     'modify_date', 'description', 'attach')
     # 文章列表里显示想要显示的字段
     list_per_page = 50
     # 满50条数据就自动分页
     ordering = ('-modify_date', )
-    #后台数据列表排序方式
+    # 后台数据列表排序方式
     list_display_links = ('id', 'name')
     # 设置哪些字段可以点击进入编辑界面
 
 
-@admin.register(CategoryShip)
-class CategoryShipAdmin(admin.ModelAdmin):
-    list_display = ('id', 'category', 'wordbook', 'create_date', 'modify_date',
-                    'description', 'attach')
+@admin.register(WordbookShip)
+class WordbookShipAdmin(admin.ModelAdmin):
+    list_display = ('id', 'item', 'parent')
     # 文章列表里显示想要显示的字段
     list_per_page = 50
     # 满50条数据就自动分页
-    ordering = ('-modify_date', )
-    #后台数据列表排序方式
+    ordering = ('-id', )
+    # 后台数据列表排序方式
     list_display_links = ('id', )
     # 设置哪些字段可以点击进入编辑界面
