caad9a822798db9056550bf32ae750f296c0d23a
Sinchard  Smith <a52581120@163.com>
2020-04-21 23:20:19 +0800

    add devicein and materialin to admin page

--- stock/admin.py
+++ stock/admin.py
@@ -1,16 +1,28 @@
 from django.contrib import admin
 
-from stock.models import Warehouse
+from stock.models import DeviceIn, MaterialIn
 
 
-@admin.register(Warehouse)
-class WarehouseAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'location', 'check', 'create_date',
+@admin.register(DeviceIn)
+class DeviceInAdmin(admin.ModelAdmin):
+    list_display = ('id', 'device', 'warehouse', 'employee', 'check', 'create_date',
                     'modify_date', 'description', 'attach')
     # 文章列表里显示想要显示的字段
     list_per_page = 50
     # 满50条数据就自动分页
     ordering = ('-modify_date', )
-    #后台数据列表排序方式
-    list_display_links = ('id', 'name')
+    # 后台数据列表排序方式
+    list_display_links = ('id', 'device')
+    # 设置哪些字段可以点击进入编辑界面
+
+
+class MaterialInAdmin(admin.ModelAdmin):
+    list_display = ('id', 'material', 'amount', 'warehouse', 'employee', 'check', 'create_date',
+                    'modify_date', 'description', 'attach')
+    # 文章列表里显示想要显示的字段
+    list_per_page = 50
+    # 满50条数据就自动分页
+    ordering = ('-modify_date', )
+    # 后台数据列表排序方式
+    list_display_links = ('id', 'material')
     # 设置哪些字段可以点击进入编辑界面
