55cc3772ef06c9d3c99dc9341f718fb3bc633eb3
Sinchard <sinchard420@gmail.com>
2020-04-23 14:18:51 +0800

    add application operation views

--- stock/urls.py
+++ stock/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from stock.views import DeviceInList, DeviceInCreate, DeviceInUpdate, DeviceInDelete
+from stock.views import ApplicationList, ApplicationCreate, ApplicationUpdate, ApplicationDelete
 
 app_name = 'stock'
 
@@ -8,4 +9,9 @@ urlpatterns = [
     path('devicein/add', DeviceInCreate.as_view(), name='add_devicein'),
     path('devicein/<int:pk>/', DeviceInUpdate.as_view(), name='edit_devicein'),
     path('devicein/<int:pk>/delete', DeviceInDelete.as_view(), name='delete_devicein'),
+
+    path('applications', ApplicationList.as_view(), name='applications'),
+    path('application/add', ApplicationCreate.as_view(), name='add_application'),
+    path('application/<int:pk>/', ApplicationUpdate.as_view(), name='edit_application'),
+    path('application/<int:pk>/delete', ApplicationDelete.as_view(), name='delete_application'),
 ]
