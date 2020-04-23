32062b55306afdd2ee6c593a9c43c10b84271820
Sinchard <sinchard420@gmail.com>
2020-04-23 13:59:34 +0800

    register Application in admin page

--- stock/admin.py
+++ stock/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from stock.models import DeviceIn, MaterialIn
+from stock.models import DeviceIn, MaterialIn, Application, ApplicationDetail
 
 
 @admin.register(DeviceIn)
@@ -26,4 +26,17 @@ class MaterialInAdmin(admin.ModelAdmin):
     ordering = ('-modify_date', )
     # 后台数据列表排序方式
     list_display_links = ('id', 'material')
+    # 设置哪些字段可以点击进入编辑界面
+
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'applicant', 'status', 'approver', 'approve_content', 'approve_date',
+                    'confirmed','confirm_content','confirm_date', 'description', 'attach')
+    # 文章列表里显示想要显示的字段
+    list_per_page = 50
+    # 满50条数据就自动分页
+    ordering = ('-modify_date', )
+    # 后台数据列表排序方式
+    list_display_links = ('id', 'name')
     # 设置哪些字段可以点击进入编辑界面
