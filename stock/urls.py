bab78a3b5ed3d580b0c9511782001f9c3f581340
Sinchard <sinchard420@gmail.com>
2020-04-27 10:13:12 +0800

    add device in and operate urls

--- stock/urls.py
+++ stock/urls.py
@@ -5,10 +5,18 @@ from stock.views import ApplicationList, ApplicationCreate, ApplicationUpdate, A
 app_name = 'stock'
 
 urlpatterns = [
+    # display all device in and out records
     path('DeviceOperates', DeviceOperateList.as_view(), name='DeviceOperates'),
-    path('DeviceOperate/add', DeviceOperateCreate.as_view(), name='add_DeviceOperate'),
-    path('DeviceOperate/<int:pk>/', DeviceOperateUpdate.as_view(), name='edit_DeviceOperate'),
-    path('DeviceOperate/<int:pk>/delete', DeviceOperateDelete.as_view(), name='delete_DeviceOperate'),
+    # device in operate
+    path('DeviceIns', DeviceOperateList.as_view(), {'operate': 1}, name='DeviceIns'),
+    path('DeviceIn/add', DeviceOperateCreate.as_view(), {'operate': 1}, name='add_DeviceIn'),
+    path('DeviceIn/<int:pk>/', DeviceOperateUpdate.as_view(), {'operate': 1}, name='edit_DeviceIn'),
+    path('DeviceIn/<int:pk>/delete', DeviceOperateDelete.as_view(), {'operate': 1}, name='delete_DeviceIn'),
+    # device out operate
+    path('DeviceOuts', DeviceOperateList.as_view(), {'operate': 2}, name='DeviceOuts'),
+    path('DeviceOut/add', DeviceOperateCreate.as_view(), {'operate': 2}, name='add_DeviceOut'),
+    path('DeviceOut/<int:pk>/', DeviceOperateUpdate.as_view(), {'operate': 2}, name='edit_DeviceOut'),
+    path('DeviceOut/<int:pk>/delete', DeviceOperateDelete.as_view(), {'operate': 2}, name='delete_DeviceOut'),
 
     path('applications', ApplicationList.as_view(), name='applications'),
     path('application/add', ApplicationCreate.as_view(), name='add_application'),
