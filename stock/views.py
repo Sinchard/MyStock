5b3de820a3797c0cb192c58dfb82fbf90f5cc6c4
Sinchard <sinchard420@gmail.com>
2020-04-28 16:49:57 +0800

    修改设备入库列表URL

--- stock/views.py
+++ stock/views.py
@@ -22,7 +22,7 @@ class DeviceOperateList(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(DeviceOperateList, self).get_context_data(**kwargs)
-        context['theurl'] = reverse_lazy("basic:wordbooks")
+        context['theurl'] = reverse_lazy("stock:deviceins")
         context['operate']=self.kwargs["operate"]
         return context    
 
