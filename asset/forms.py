26b3359dba9e2297071f385d912a3f43d3d26bf8
Sinchard <sinchard420@gmail.com>
2020-04-28 11:10:13 +0800

    为Device模型添加Autocomplate控件

--- asset/forms.py
+++ asset/forms.py
@@ -1,16 +1,42 @@
-from django.forms import ModelForm
+from django import forms
+from django.forms import TextInput, Textarea, Select
+
+from dal import autocomplete
 
 from asset.models import Device, Material
 
 
-class DeviceForm(ModelForm):
+class DeviceForm(forms.ModelForm):
+    id = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    value = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}), required=False, initial=0)
+    asset = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}), required=False)
+    sap = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}), required=False)
+    description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': '2'}), required=False)
+    # An inline class to provide additional information on the form.
     class Meta:
+        # Provide an association between the ModelForm and a model
         model = Device
-        fields = ['id', 'brand', 'model', 'sn', 'classification', 'model', 'value', 'asset', 'sap', 'status',
-                    'description']
+        fields = ('id', 'classification', 'brand', 'model', 'sn', 'value', 'asset', 'sap', 'good','description')
+        widgets = {
+            'classification': autocomplete.ModelSelect2(
+                        url='basic:wordbook-autocomplete',
+                        attrs={'class': 'form-control'}
+                    ),
+            'brand': autocomplete.ModelSelect2(
+                        url='basic:wordbook-autocomplete',
+                        attrs={'class': 'form-control'},
+                        forward=['type'],
+                    ),
+            'model': autocomplete.ModelSelect2(
+                        url='basic:wordbook-autocomplete',
+                        attrs={'class': 'form-control'},
+                        forward=['name'],
+                    ),
+            'sn': TextInput(attrs={'class': 'form-control'}),
+            'good': Select(attrs={'class': 'form-control'}),
+        }
 
-
-class MaterialForm(ModelForm):
+class MaterialForm(forms.ModelForm):
     class Meta:
         model = Material
         exclude = []
